74be2aa0659b18ab8510e19dedb10d1e7962ae43
Krzysztof Chomicz <krzysztof.chomicz@witkac.pl>
2018-05-10 11:47:17 +0200

    WIP

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.use("/", router);
 const server = app.listen(app.get("port"), () => {
   console.log("  App is running at http://localhost:%d in %s mode", app.get("port"), app.get("env"));
   console.log("  Press CTRL-C to stop\n");
-  console.log("configuration:", settings);
+  console.log("  Configuration:", settings);
 });
 
 export default server;
